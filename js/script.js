90a620ba8a2b8a7fa43c07f4e965ab00b073aa5d
golovanovv <46806234+golovanovv@users.noreply.github.com>
2019-03-13 16:25:09 +0400

    Сделал попап обратной связи на главной и поправил его стилизацию

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var miniMap = document.querySelector('.map');
 var popupMap = document.querySelector('.modal-map');
-var closeMap = document.querySelector('.modal-close');
+var closeMap = document.querySelector('.close-map');
+var closeFeedback = document.querySelector('.close-feedback');
+var writeUs = document.querySelector('.info-button-write-us');
+var popupFeedback = document.querySelector('.modal-feedback');
 
 miniMap.addEventListener('click', function () {
 	popupMap.classList.add('modal-map-open');
@@ -8,4 +11,12 @@ miniMap.addEventListener('click', function () {
 
 closeMap.addEventListener('click', function () {
 	popupMap.classList.remove('modal-map-open');
+});
+
+writeUs.addEventListener('click', function () {
+	popupFeedback.classList.add('modal-feedback-open');
+});
+
+closeFeedback.addEventListener('click', function () {
+	popupFeedback.classList.remove('modal-feedback-open');
 });
