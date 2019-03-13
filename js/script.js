f0de45682e0dc9851753df08cc7dbaa0add2fde4
golovanovv <46806234+golovanovv@users.noreply.github.com>
2019-03-13 17:21:51 +0400

    Сделал попап добавления товара в корзину на странице каталога

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ var closeMap = document.querySelector('.close-map');
 var closeFeedback = document.querySelector('.close-feedback');
 var writeUs = document.querySelector('.info-button-write-us');
 var popupFeedback = document.querySelector('.modal-feedback');
+var buy = document.querySelector('.buy');
+var popupBuy = document.querySelector('.item-add');
+var closeAdd = document.querySelector('.close-add');
 
 miniMap.addEventListener('click', function () {
 	popupMap.classList.add('modal-map-open');
@@ -19,4 +22,12 @@ writeUs.addEventListener('click', function () {
 
 closeFeedback.addEventListener('click', function () {
 	popupFeedback.classList.remove('modal-feedback-open');
+});
+
+buy.addEventListener('click', function () {
+	popupBuy.classList.add('modal-add-open');
+});
+
+closeAdd.addEventListener('click', function () {
+	popupBuy.classList.remove('modal-add-open');
 });
